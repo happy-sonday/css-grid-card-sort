3f8393ac81d3e74845ebcc1767c631dbc4e8973d
sws <sws@paywith.co.kr>
2021-02-21 02:43:39 +0900

    1m gird를 활용한 카드리스트 정렬

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'css-grid-pt';
+  isChecked = false;
+  constructor() {}
+
+  onClick() {
+    return (this.isChecked = !this.isChecked);
+  }
 }
